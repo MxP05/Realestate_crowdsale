cf161e51d2f4a8397993a1136177ae040aecb713
12jasteiner <117539488+12jasteiner@users.noreply.github.com>
2023-05-15 02:27:21 -0400

    NFT ERC721 contract
    contracts imported. NFT created to store and verify property info. NFT cannot be traded in app UI

---
+++ Jason/MyNFT.sol
@@ -0,0 +1,30 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//track total # nfts minted and set uiniqe id new nft
+import "@openzeppelin/contracts/utils/Counters.sol";
+//only owner can mint nfts
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+//name and symbol NFT
+    constructor() ERC721("PropertyInfoNFT", "PIN") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
